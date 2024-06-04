4d2475e11466388eaac9fe8aa7e563c1731908b2
thisisxjj <763801840@qq.com>
2024-06-04 19:07:09 +0800

    - 完成app layout布局

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Users from './pages/Users'
 import Settings from './pages/Settings'
 import Account from './pages/Account'
 import Login from './pages/Login'
+import AppLayout from './ui/AppLayout'
 import PageNotFound from './pages/PageNotFound'
 
 function App() {
@@ -15,13 +16,15 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Navigate replace to="dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="bookings" element={<Bookings />} />
-          <Route path="cabins" element={<Cabins />} />
-          <Route path="users" element={<Users />} />
-          <Route path="settings" element={<Settings />} />
-          <Route path="account" element={<Account />} />
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
