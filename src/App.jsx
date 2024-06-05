2d0267fa9ee893f5277b1af23a42f77a5989ad51
thisisxjj <763801840@qq.com>
2024-06-05 19:14:57 +0800

    - 完成Toast初始化

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Account from './pages/Account'
 import Login from './pages/Login'
 import AppLayout from './ui/AppLayout'
 import PageNotFound from './pages/PageNotFound'
+import { Toaster } from 'react-hot-toast'
 
 // Create a client
 const queryClient = new QueryClient({
@@ -45,6 +46,27 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: '8px' }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 3000,
+          },
+          style: {
+            fontSize: '16px',
+            maxWidth: '500px',
+            padding: '16px 24px',
+            backgroundColor: 'var(--color-grey-0)',
+            color: 'var(--color-grey-700)',
+          },
+        }}
+      />
     </QueryClientProvider>
   )
 }
--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 import { formatCurrency } from '../../utils/helpers'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { deleteCabin } from '../../services/apiCabins'
+import toast from 'react-hot-toast'
 
 const TableRow = styled.div`
   display: grid;
@@ -55,11 +56,16 @@ function CabinRow({ cabin }) {
   const queryClient = useQueryClient()
 
   const { isLoading: isDeleting, mutate } = useMutation({
-    mutationFn: (id) => deleteCabin(id),
-    onSuccess: () =>
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success('Cabin successfully deleted!')
       queryClient.invalidateQueries({
         queryKey: ['cabins'],
-      }),
+      })
+    },
+    onError: (error) => {
+      toast.error(`Cabin could not be deleted! ${error.message}`)
+    },
   })
   return (
     <TableRow role="row">
